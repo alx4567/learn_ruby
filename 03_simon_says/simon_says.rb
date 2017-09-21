4ff9ce9e20cefc0c11d227c778e0a5c382421e3b
Alex Fore <forealexander4@gmail.com>
2017-09-21 17:18:37 -0500

    03_simon_says problems solved

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,36 @@
-#write your code here
+def echo(i)
+  i
+end
+
+def shout(i)
+  i.upcase
+end
+
+def repeat(i, b=1)
+  if b == 1
+    i + ' ' + i
+  else
+    i + ((' ' + i) * (b-1))
+  end
+end
+
+def start_of_word(x, y)
+  x[0, y]
+end
+
+def first_word(x)
+  y = x.split
+  y[0]
+end
+
+def titleize(x)
+  words = x.split.map do |word|
+    if %w(the and over).include?(word)
+      word
+    else
+      word.capitalize
+    end
+  end
+  words.first.capitalize!
+  words.join(" ")
+end
