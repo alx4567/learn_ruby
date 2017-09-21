8eb8b5e082e3d14fbbde4937e802f5024f4ee502
Alex Fore <forealexander4@gmail.com>
2017-09-21 15:02:00 -0500

    solved 02_calculator problems

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,37 @@
 #write your code here
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def sum(array)
+  sum = 0.0
+  if array.length > 0 then
+    array.each do |item|
+      sum += item
+    end
+  end
+  return sum
+end
+
+def multiply(*array)
+  mult = 1
+  array.each { |i| mult *= i }
+  mult
+end
+
+def power(a, b)
+  a**b
+end
+
+def factorial(a)
+  total = a
+  while a > 1
+    a -= 1
+    total = total * a
+  end
+  total
+end
